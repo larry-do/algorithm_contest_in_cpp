f0777fdb1cc840a8a03fe288a5d99a47c20accee
Dou_Ding_Nyat <nhatkap2@gmail.com>
2017-08-22 21:44:22 -0700

    a

--- horner.cpp
+++ horner.cpp
@@ -3,7 +3,7 @@
 int horner(int a[],int n,int x);
 int hornerRecursive(int a[],int n,int x,int pos);
 int main(){
-    int b[]={4,5,6},n=3,x=2; //khai báo của người dùng, danh sách hệ số, số hệ số, và hệ số nhân x
+    int b[]={4,5},n=2,x=2; //khai báo của người dùng, danh sách hệ số, số hệ số, và hệ số nhân x
     std::cout<<horner(b,n,x)<<std::endl;
     std::cout<<hornerRecursive(b,n,x,n-1)<<std::endl;
     return 0;
@@ -19,6 +19,7 @@ int horner(int a[],int n,int x){
 }
 int hornerRecursive(int a[],int n,int x,int pos){
     if(n<=0) return 0;
+    if(n==1) return a[0]*x;
     if(pos==0) return a[0];
     return a[pos]+x*hornerRecursive(a,n,x,--pos);
 }
